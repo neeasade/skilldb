6831e63f74024cec7ceab69a1c8d0199c6296348
Nathan Isom <nathanisom27@gmail.com>
2015-11-11 15:52:17 -0600

    Added angular ng-table package, got basic table with sorting worth it.

--- skilldb.js
+++ skilldb.js
@@ -1,9 +1,19 @@
 //Employees = new Mongo.Collection("employees");
 
 if (Meteor.isClient) {
-  angular.module('skilldb', ['angular-meteor']);
+  angular.module('skilldb', ['angular-meteor', 'ngTable']);
 
   angular.module('skilldb').controller('EmployeeCtrl', function ($scope, $meteor) {
     $scope.employees = $meteor.collection(Employees);
+
+	$scope.$inject = ["NgTableParams"];
+    function demoController(NgTableParams, simpleList) {
+          var self = this;
+          self.tableParams = new NgTableParams({}, {
+                  dataset: simpleList
+          });
+    };
+
   });
+
 }
