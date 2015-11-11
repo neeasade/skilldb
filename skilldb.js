8fbc4e55eaab76bc9654a621ecd3f26fa0955f4f
Nathan Isom <nathanisom27@gmail.com>
2015-11-11 10:01:54 -0600

    Basic lists working.

--- skilldb.js
+++ skilldb.js
@@ -1,23 +1,14 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  angular.module('skilldb', ['angular-meteor']);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  angular.module('skilldb').controller('EmployeeCtrl', ['$scope', function ($scope) {
+    $scope.employees = [
+      {
+        'name': 'Bob Smith',
+      },
+      {
+        'name': 'Sarah Wells',
+      }
+    ];
+  }]);
 }
