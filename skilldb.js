28dabded1c983e0bf52a78ee55007f31149bca11
Nathan Isom <nathanisom27@gmail.com>
2015-11-12 11:02:33 -0600

    correctly padding parameters to ng-table now.

--- skilldb.js
+++ skilldb.js
@@ -1,19 +1,25 @@
-//Employees = new Mongo.Collection("employees");
 
 if (Meteor.isClient) {
-  angular.module('skilldb', ['angular-meteor', 'ngTable']);
-
-  angular.module('skilldb').controller('EmployeeCtrl', function ($scope, $meteor) {
+  angular.module('skilldb', ['ngTable', 'angular-meteor'])
+  .controller('EmployeeCtrl', ['$scope', '$meteor', '$filter', 'ngTableParams',
+  function EmployeeCtrl($scope, $meteor, $filter, NgTableParams) {
+    // relate the monogodb collections
     $scope.employees = $meteor.collection(Employees);
+    $scope.skills = $meteor.collection(Skills);
+    $scope.titles = $meteor.collection(Titles);
+    $scope.locations = $meteor.collection(Locations);
+    $scope.clients = $meteor.collection(Clients);
 
-	$scope.$inject = ["NgTableParams"];
-    function demoController(NgTableParams, simpleList) {
-          var self = this;
-          self.tableParams = new NgTableParams({}, {
-                  dataset: simpleList
-          });
-    };
-
-  });
-
+    $scope.tableParams = new NgTableParams({
+      //count: 1,
+      sorting: {
+        name: 'asc'
+      }
+    }, {
+      //dataset: temp
+      getData: function($defer, params) {
+          $defer.resolve($filter('orderBy')($scope.employees, params.orderBy()));
+      }
+    });
+  }]);
 }
